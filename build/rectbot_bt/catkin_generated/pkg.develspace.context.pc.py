09dc1858545e0b2f85d749768930fef0a6a4f1d1
khanh <nguyennhakhanh2003@gmail.com>
2025-05-26 22:47:27 +0700

    fix git ignore

---
+++ build/rectbot_bt/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/SLAM/rectbot_ws/src/rectbot_bt/include".split(';') if "/SLAM/rectbot_ws/src/rectbot_bt/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;rospy;nav_msgs;geometry_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "rectbot_bt"
+PROJECT_SPACE_DIR = "/SLAM/rectbot_ws/devel"
+PROJECT_VERSION = "0.0.0"
